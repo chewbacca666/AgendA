1983c779084c17d285c106abfafd5a485f518fec
chewbacca666 <32313920+chewbacca666@users.noreply.github.com>
2018-04-12 14:58:58 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,77 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform, ToastController, Icon } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { HomePage } from '../pages/eventos/home';
+import { ListPage } from '../pages/list/list';
+import { ConfigPage } from '../pages/sidemenu/config/config';
+import { SairPage } from '../pages/sidemenu/sair/sair';
+import { MeusPage } from '../pages/sidemenu/meus/meus';
+import { PerfilPage } from '../pages/sidemenu/perfil/perfil';
+import { PainelPage } from '../pages/sidemenu/painel/painel';
+import { InicioPage } from '../pages/inicio/inicio';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  [x: string]: any;
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = InicioPage;
+
+  pages: Array<{title: string, component: any}>;
+  public 
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public toastCtrl: ToastController) {
+    /*this.initializeApp();*/
+    platform.ready().then(() => {
+      statusBar.styleDefault();
+      splashScreen.hide();
+ 
+      platform.registerBackButtonAction(() => {
+        if (this.counter == 0) {
+          this.counter++;
+          this.presentToast();
+          setTimeout(() => { this.counter = 0 }, 3000)
+        } else {
+          console.log("exitapp");
+          platform.exitApp();
+        }
+      }, 0)
+    });
+  
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Painel', component: PainelPage},
+      { title: 'Perfil', component: PerfilPage},
+      { title: 'Meus eventos', component: HomePage}, 
+      { title: 'List', component: ListPage},
+      { title: 'Meus grupos', component:MeusPage},
+      { title: 'Configuração', component: ConfigPage},
+      { title: 'Sair', component: SairPage},
+    ];
+
+  }
+  presentToast(){
+    let toast = this.toastCtrl.create({
+      message: "Press again to exit",
+      duration: 3000,
+      position: "middle"
+    })
+    toast.present();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/eventos/home';
+import { ListPage } from '../pages/list/list';
+import { NgCalendarModule  } from 'ionic2-calendar';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ConfigPage } from '../pages/sidemenu/config/config';
+import { MeusPage } from '../pages/sidemenu/meus/meus';
+import { PerfilPage } from '../pages/sidemenu/perfil/perfil';
+import { PainelPage } from '../pages/sidemenu/painel/painel';
+import { SairPage } from '../pages/sidemenu/sair/sair';
+import { InicioPage } from '../pages/inicio/inicio';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    ConfigPage,
+    MeusPage,
+    PainelPage,
+    PerfilPage,
+    SairPage,
+    InicioPage,
+  ],
+  imports: [
+    NgCalendarModule,
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    ConfigPage,
+    MeusPage,
+    PerfilPage,
+    PainelPage,
+    SairPage,
+    InicioPage,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
